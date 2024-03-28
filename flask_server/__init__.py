69f9c7bc73ea9d360150185d65be3441cf8ff971
Joe Egboka <joeegboka@gmail.com>
2024-03-28 17:24:02 +0100

    connect and test database communication with app

---
+++ flask_server/__init__.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import time
+from os import environ
+import json
+
+from dotenv import load_dotenv
+from flask import Flask, jsonify, make_response, request
+from flask_cors import CORS
+from gotrue.errors import AuthApiError, AuthRetryableError
+from supabase import create_client
+
+
+app = Flask(__name__)
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
+# app.register_blueprint(app_views)
+# cor = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cor = CORS(app)
--- flask_server/my_app.py
+++ flask_server/my_app.py
@@ -1,16 +1,54 @@
-"""signup module"""
-from flask_server.auth.v1 import (
+"""Application entry point"""
+
+from . import (
     AuthApiError,
     AuthRetryableError,
     app,
+    create_client,
+    environ,
     json,
     jsonify,
+    load_dotenv,
+    make_response,
     request,
-    supabase,
     time,
 )
 
+load_dotenv()
+url = environ.get("SUPABASE_URL")
+key = environ.get("SUPABASE_API_KEY")
 
+supabase = create_client(url, key)
+
+
+# Routes and other application logic go here
+@app.errorhandler(404)
+def not_found(error):
+    """404 Error
+    ---
+    responses:
+      404:
+        description: a resource was not found
+    """
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+# check status route
+@app.route("/status", methods=["GET"], strict_slashes=False)
+def status():
+    """Status of API"""
+    return jsonify({"status": "OK"})
+
+
+# api routes
+@app.route("/api/v1/patients", methods=["GET"], strict_slashes=False)
+def patients():
+    """get all users"""
+    data = supabase.table("patients").select("*").execute()
+
+    return data.model_dump_json()
+
+
+# authentication routes
 @app.route("/auth/v1/signup", methods=["POST"], strict_slashes=False)
 def signup():
     """signup function"""
@@ -39,13 +77,23 @@ def signup():
                         "category": user_data.get("category"),
                         "age": user_data.get("age"),
                         "specialization": user_data.get("specialization"),
-                        "dob": user_data.get("DOB"),
+                        "dob": user_data.get("dob"),
                         "gender": user_data.get("gender"),
                     }
                 )
                 .eq("id", user_id)
                 .execute()
             )
+            print(data.model_dump_json())
         return data.model_dump_json()
     except (AuthApiError, AuthRetryableError) as error:
         return jsonify({"message": "Sign up failed!", "error": error.message})
+
+
+if __name__ == "__main__":
+    HOST = environ.get("SERVER_HOST", "localhost")
+    PORT = int(environ.get("SERVER_PORT", 5555))
+
+    print(f"Server running on {HOST}:{PORT}")
+
+    app.run(debug=True, host=HOST, port=PORT)
