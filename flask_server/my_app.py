d23b86e84f906d3b60ee7b12ee3d3dde6cec7a41
Joe Egboka <joeegboka@gmail.com>
2024-03-29 16:50:55 +0100

    create medical record routes and authentication routes

--- flask_server/my_app.py
+++ flask_server/my_app.py
@@ -21,7 +21,7 @@ key = environ.get("SUPABASE_API_KEY")
 supabase = create_client(url, key)
 
 
-# Routes and other application logic go here
+# error handler
 @app.errorhandler(404)
 def not_found(error):
     """404 Error
@@ -38,8 +38,7 @@ def status():
     """Status of API"""
     return jsonify({"status": "OK"})
 
-
-# api routes
+######################      api routes for patients      ######################
 @app.route("/api/v1/patients", methods=["GET"], strict_slashes=False)
 def patients():
     """get all users"""
@@ -47,8 +46,75 @@ def patients():
 
     return data.model_dump_json()
 
+#######################      api routes for medics      #######################
 
-# authentication routes
+
+###################      api route for medical history     ####################
+# create patient's medical record
+@app.route(
+    "/api/v1/patients/<patient_id>/medical_record",
+    methods=["POST"],
+    strict_slashes=False,
+)
+def create_medical_history(patient_id):
+    """create medical record"""
+    req_data = request.get_json()
+    req_data["patient_id"] = patient_id
+    data = supabase.table("medical_record").insert(req_data).execute()
+    return data.model_dump_json()
+
+
+# get patient's medical history
+@app.route(
+    "/api/v1/patients/<patient_id>/medical_record",
+    methods=["GET"],
+    strict_slashes=False,
+)
+def get_medical_history(patient_id):
+    """get medical record"""
+    data = (
+        supabase.table("medical_record")
+        .select("*")
+        .eq("patient_id", patient_id)
+        .execute()
+    )
+    return data.model_dump_json()
+
+
+# update patient's medical history
+@app.route(
+    "/api/v1/patients/<patient_id>/medical_record",
+    methods=["PATCH"],
+    strict_slashes=False,
+)
+def update_medical_history(patient_id):
+    """update medical record"""
+    req_data = request.get_json()
+    data = (
+        supabase.table("medical_record")
+        .update(req_data)
+        .eq("patient_id", patient_id)
+        .execute()
+    )
+    return data.model_dump_json()
+
+
+# delete patient's medical history
+@app.route(
+    "/api/v1/patients/<patient_id>/medical_record",
+    methods=["DELETE"],
+    strict_slashes=False,
+)
+def delete_medical_history(patient_id):
+    """delete medical record"""
+    data = (
+        supabase.table("medical_record").delete().eq("patient_id", patient_id).execute()
+    )
+    return data.model_dump_json()
+
+
+########################   authentication routes   #############################
+# sign up route
 @app.route("/auth/v1/signup", methods=["POST"], strict_slashes=False)
 def signup():
     """signup function"""
@@ -88,6 +154,36 @@ def signup():
         return data.model_dump_json()
     except (AuthApiError, AuthRetryableError) as error:
         return jsonify({"message": "Sign up failed!", "error": error.message})
+
+
+# sign in route
+@app.route("/auth/v1/signin", methods=["POST"], strict_slashes=False)
+def signin():
+    """signin function"""
+    session = None
+    try:
+        user_data = request.get_json()
+        session = supabase.auth.sign_in_with_password(
+            {
+                "email": user_data.get("email"),
+                "password": user_data.get("password"),
+            }
+        )
+        return session.model_dump_json()
+    except (AuthApiError, AuthRetryableError) as error:
+        return jsonify({"message": "Sign in failed!", "error": error.message})
+
+
+# sign out route
+@app.route("/auth/v1/signout", methods=["POST"], strict_slashes=False)
+def signout():
+    """signout function"""
+    try:
+        user_data = request.get_json()
+        supabase.auth.sign_out(user_data.get("access_token"))
+        return jsonify({"message": "Sign out successful!"})
+    except (AuthApiError, AuthRetryableError) as error:
+        return jsonify({"message": "Sign out failed!", "error": error.message})
 
 
 if __name__ == "__main__":
