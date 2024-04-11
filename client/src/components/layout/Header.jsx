3b2956b14bb858d47895e9ee4e1a6b14e1996213
Joe Egboka <joeegboka@gmail.com>
2024-04-11 23:26:48 +0100

    clean-up codebase

--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,51 +1,57 @@
-import { Link, useNavigate } from "react-router-dom";
-import logo from "../../assets/images/logo.png"
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.png";
 
 import MenuIcon from "@mui/icons-material/Menu";
 import { MenuOpen } from "@mui/icons-material";
 import HomeIcon from "@mui/icons-material/Home";
 import axios from "axios";
 
-const Header = (props) => {
-  const navigate = useNavigate();
-
-  let userId;
-  if (props.token) {
-    userId = props?.decodedToken.sub;
-  }
-
+const Header = ({
+  setMenuVisible,
+  menuVisible,
+  token,
+  setToken,
+  activePage,
+  setActivePage,
+  setMedicalRecord,
+  setDecodedToken,
+}) => {
   const apiURL = import.meta.env.VITE_API_BASE_URL;
 
   const reqURL = `${apiURL}/auth/v1/signout`;
 
   const toggleMenuHandler = (e) => {
     e.stopPropagation();
-    props.setMenuVisible(!props.menuVisible);
+    setMenuVisible(!menuVisible);
   };
 
   const handleSignout = () => {
-    axios.post(reqURL);
     localStorage.removeItem("access_token");
-    props.setToken(null);
+    setToken(null);
+    setMedicalRecord(null);
+    setDecodedToken(null);
+    axios.post(reqURL);
   };
 
   return (
     <header className="bg-blue-700 sticky top-0 z-20">
       <div className="max-w-7xl mx-auto items-center flex justify-between p-2.5">
-        <div onClick={() => props.setActivePage("home")}>
+        <div onClick={() => setActivePage("home")}>
           <Link to="/">
             <span className="flex md:hidden text-slate-200">
               <HomeIcon />
             </span>
-            <span className="hidden text-white md:flex"><img src={logo} alt="logo" className="w-12 rounded-full" /></span>
+            <span className="hidden text-white md:flex">
+              <img src={logo} alt="logo" className="w-12 rounded-full" />
+            </span>
           </Link>
         </div>
         <div onClick={toggleMenuHandler} className="md:hidden text-slate-200">
-          {props.menuVisible ? <MenuOpen /> : <MenuIcon />}
+          {menuVisible ? <MenuOpen /> : <MenuIcon />}
         </div>
         <nav
           className={`${
-            !props.menuVisible && "hidden"
+            !menuVisible && "hidden"
           } absolute z-20 bg-blue-100 flex flex-col top-full right-0 p-2
           md:static md:w-auto md:bg-inherit md:p-0 md:flex`}
         >
@@ -55,10 +61,10 @@ const Header = (props) => {
                 to="/our-doctors"
                 className={`w-full flex text-base md:hover:text-blue-200 cursor-pointer
                 ${
-                  props.activePage === "our-doctors" &&
+                  activePage === "our-doctors" &&
                   "text-blue-700 md:text-blue-200"
                 }`}
-                onClick={() => props.setActivePage("our-doctors")}
+                onClick={() => setActivePage("our-doctors")}
               >
                 Doctors
               </Link>
@@ -67,11 +73,8 @@ const Header = (props) => {
               <Link
                 to="/about"
                 className={`w-full flex text-base md:hover:text-blue-200 cursor-pointer
-                ${
-                  props.activePage === "about" &&
-                  "text-blue-700 md:text-blue-200"
-                }`}
-                onClick={() => props.setActivePage("about")}
+                ${activePage === "about" && "text-blue-700 md:text-blue-200"}`}
+                onClick={() => setActivePage("about")}
               >
                 About
               </Link>
@@ -81,38 +84,34 @@ const Header = (props) => {
                 to="/patients"
                 className={`w-full flex text-base md:hover:text-blue-200 cursor-pointer
                 ${
-                  props.activePage === "services" &&
-                  "text-blue-700 md:text-blue-200"
+                  activePage === "patients" && "text-blue-700 md:text-blue-200"
                 }`}
-                onClick={() => props.setActivePage("services")}
+                onClick={() => setActivePage("patients")}
               >
                 Patients
               </Link>
             </li>
-            {props.token && (
+            {token && (
               <li className="list-none py-2 border-b border-blue-900 border-opacity-25 mr-4 md:text-white">
                 <Link
-                  to='/user-dashboard'
+                  to="/user-dashboard"
                   className={`w-full flex text-base md:hover:text-blue-200 cursor-pointer
                 ${
-                  props.activePage === "my_profile" &&
-                  "text-blue-700 md:text-blue-200"
+                  activePage === "dashboard" && "text-blue-700 md:text-blue-200"
                 }`}
-                onClick={() => props.setActivePage("my_profile")}
-              >
-                Dashboard
-              </Link>
-            </li>)}
-            {!props.token ? (
+                  onClick={() => setActivePage("dashboard")}
+                >
+                  Dashboard
+                </Link>
+              </li>
+            )}
+            {!token ? (
               <li className="list-none py-2 mr-4 md:text-white">
                 <Link
                   to="/login"
                   className={`w-full flex text-base md:hover:text-blue-200 cursor-pointer
-                ${
-                  props.activePage === "login" &&
-                  "text-blue-700 md:text-blue-200"
-                }`}
-                  onClick={() => props.setActivePage("login")}
+                ${activePage === "login" && "text-blue-700 md:text-blue-200"}`}
+                  onClick={() => setActivePage("login")}
                 >
                   Login
                 </Link>
