a1445ba774b145ab05283c4aee87985af8f57dbd
Joe Egboka <joeegboka@gmail.com>
2024-04-15 13:35:33 +0100

    containerize the client-side

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     host: true,
     strictPort: true,
-    port: 3000,
+    port: 5000,
   },
   root: ".", // Make sure this points to the directory containing `index.html`
   build: {
