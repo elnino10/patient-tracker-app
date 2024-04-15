e7f5e39c1318b887c28255f8b5d32b075c8b9b53
Joe Egboka <joeegboka@gmail.com>
2024-04-15 13:03:39 +0100

    containerize the client-side

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    strictPort: true,
+    port: 3000,
+  },
+  root: ".", // Make sure this points to the directory containing `index.html`
+  build: {
+    rollupOptions: {
+      input: "./index.html", // Path to your entry file
+    },
+  },
 });
